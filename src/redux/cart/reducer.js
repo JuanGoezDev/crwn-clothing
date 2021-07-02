4e73a68893d6ad8239120dbbc24b3c8871a8a938
JuanGoezDev <juandagy99@gmail.com>
2021-07-02 10:29:35 -0500

    cart utils implemented

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -1,19 +1,26 @@
 import { cartActionTypes } from "./types";
+import { addItemToCart } from "./utils";
 
-const INITIAL_STATE = { 
-  hidden: true
-}
+const INITIAL_STATE = {
+  hidden: true,
+  cartItems: [],
+};
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case cartActionTypes.TOGGLE_CARD_HIDDEN:
       return {
         ...state,
-        hidden: !state.hidden
-      }
+        hidden: !state.hidden,
+      };
+    case cartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
     default:
       return state;
   }
-}
+};
 
 export default cartReducer;
