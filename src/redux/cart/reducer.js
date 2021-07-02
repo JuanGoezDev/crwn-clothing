acaa1b3bd9d6e007008bfd5b29bae8db3ddd97d2
JuanGoezDev <juandagy99@gmail.com>
2021-07-02 15:16:09 -0500

    add item and remove item functionality implemented

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -1,5 +1,5 @@
 import { cartActionTypes } from "./types";
-import { addItemToCart } from "./utils";
+import { addItemToCart, removeItemToCart } from "./utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -11,12 +11,24 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case cartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
-        hidden: !state.hidden
+        hidden: !state.hidden,
       };
     case cartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: addItemToCart(state.cartItems, action.payload)
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    case cartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemToCart(state.cartItems, action.payload),
+      };
+    case cartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
       };
     default:
       return state;
